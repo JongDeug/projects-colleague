ed3da8f476e8fb3a32a579976592074bab85b266
JongDeug(linux) <pinpoint1998@gmail.com>
2022-10-28 21:55:25 +0900

    docs: update Comment

--- controllers/BoardControllers/readPostDetailController.js
+++ controllers/BoardControllers/readPostDetailController.js
@@ -5,10 +5,11 @@ const getMethod = async (req, res, next) => {
     try {
         const getPostId = req.params.postId;
 
+        // DB에서 읽어오는데 Post, Comment 둘다 읽어와야함.
         const result = await Post.findById(getPostId).exec();
         console.log(result);
 
-        const responseData = responseDataForm(null, "readPostDetail get request complete");
+        const responseData = responseDataForm(null, "readPostDetail get request complete", result);
         res.status(200).json({ responseData });
     } catch (err) {
         next(err);
