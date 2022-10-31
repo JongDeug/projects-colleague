44b5010446d49d104f1361e51fc2a2c230832974
JongDeug <pinpoint1998@gmail.com>
2022-10-31 20:22:06 +0900

    Docs: Update verifyJWT

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -2,14 +2,15 @@ const jwt = require('jsonwebtoken');
 
 const verifyJWT = (req, res, next) => {
     const authHeader = req.headers.authorization || req.headers.Authorization;
+    let token = "쓰레기 값";
     console.log(`authHeader : ${authHeader}`);
-    if (!authHeader?.startsWith('Bearer ')) return res.status(401).json({ 'message': 'fail verify' });
+    if (authHeader?.startsWith('Bearer ')) {
+        token = authHeader.split(' ')[1];
+    }
+    // return res.status(401).json({ 'message': 'fail verify' });
 
-    const token = authHeader.split(' ')[1];
-
-    const baseUrl = req.baseUrl;
-
-    if (baseUrl.split('/')[1] === 'api') {
+    const url = req.url;
+    if (url.split('/')[1] === 'api') {
         jwt.verify(
             token, // accessToken
             process.env.ACCESS_TOKEN_SECRET, // .env 파일, accessToken 확인을 위해 맨처음 만든 암호 코드
@@ -17,7 +18,6 @@ const verifyJWT = (req, res, next) => {
                 if (err) {
                     return res.status(403).json({ 'message': 'invalid token' });
                 }
-
                 // decoded 해서 req에 원하는 값 넣어서 나중에 사용 가능.
                 req.userId = decoded.UserInfo.userId;
                 req.roles = decoded.UserInfo.roles;
