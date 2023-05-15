05539c9ce64810fd5b090439be62d6b1032ca016
JongDeug <jongdeug2021@gmail.com>
2023-05-16 01:03:11 +0900

    webrtc test

--- server/src/arena.config.ts
+++ server/src/arena.config.ts
@@ -28,10 +28,17 @@ export default Arena({
 		];
 
 		const server = createServer(app);
+		// const io = new Server(server, {
+		// 	cors: {
+		// 		origin: allowedOrigins, //specific origin you want to give access to,
+		// 	}
+		// });
+
 		const io = new Server(server, {
 			cors: {
-				origin: allowedOrigins, //specific origin you want to give access to,
-			}
+				origin: allowedOrigins,
+				credentials: true,
+			},
 		});
 
 		/**
