d96fbbd92666f6fcc2b847c31ae42e879f0f25ae
SSH4240 <pdxucxy@gmail.com>
2023-05-29 18:38:16 +0900

    minutes, calendar

--- src/main/java/com/joinus/joinus/domain/Calendar.java
+++ src/main/java/com/joinus/joinus/domain/Calendar.java
@@ -6,15 +6,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
 
-import java.time.LocalDateTime;
-
 @Data
 @Entity
 public class Calendar {
     @Id @GeneratedValue
     private Long id;
     private String title;
-    private LocalDateTime startDate;
-    private LocalDateTime endDate;
+    private String startDate;
+    private String endDate;
     private Long teamId;
 }
