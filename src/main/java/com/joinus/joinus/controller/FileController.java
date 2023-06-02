6de9e25d03d1f5225e4448caf1dd84d89ee966e4
SSH4240 <pdxucxy@gmail.com>
2023-06-02 12:22:15 +0900

    test

--- src/main/java/com/joinus/joinus/controller/FileController.java
+++ src/main/java/com/joinus/joinus/controller/FileController.java
@@ -2,6 +2,7 @@ package com.joinus.joinus.controller;
 
 import com.joinus.joinus.service.FileService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +12,18 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/api/file")
 @RequiredArgsConstructor
+@Slf4j
 public class FileController {
     private final FileService fileService;
 
     @PostMapping("/upload")
     public String upload(@RequestBody MultipartFile[] uploadFile){
+        String uploadPath = "C:\\Users\\user\\Desktop\\Spring\\join-us-web-back-end\\src\\main\\resources\\";
 
+        for (MultipartFile multipartFile : uploadFile){
+            log.info("#############################");
+            log.info("info = {}", multipartFile.getOriginalFilename());
+        }
         return "success";
     }
 }
--- src/main/java/com/joinus/joinus/service/FileService.java
+++ src/main/java/com/joinus/joinus/service/FileService.java
@@ -1,9 +1,35 @@
 package com.joinus.joinus.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class FileService {
+    public String uploadFile(MultipartFile[] uploadFile, String type, String id){
+        String uploadPath = "C:\\Users\\user\\Desktop\\Spring\\join-us-web-back-end\\src\\main\\resources\\" + type + "\\" + id;
+        String fileName;
+
+        for (MultipartFile multipartFile : uploadFile){
+            log.info("#############################");
+            log.info("파일명 : ", multipartFile.getOriginalFilename());
+            log.info("파일 크기 : ", multipartFile.getSize());
+
+            File saveFile = new File(uploadPath, multipartFile.getOriginalFilename());
+            fileName = multipartFile.getOriginalFilename();
+            try{
+                multipartFile.transferTo(saveFile);
+            }
+            catch (Exception e){
+                log.error(e.getMessage());
+            }
+        }
+        return "success";
+
+    }
 }
